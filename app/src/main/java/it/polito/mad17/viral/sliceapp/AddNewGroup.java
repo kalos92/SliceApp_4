d700f9707803442b1c5656c81d1c55f84208caf7
kalos92 <kalos1992@hotmail.it>
2017-04-27 15:48:17 +0200

    Abbiamo aggiunto i permessi per accedere ai dati in rubrica

--- app/src/main/java/it/polito/mad17/viral/sliceapp/AddNewGroup.java
+++ app/src/main/java/it/polito/mad17/viral/sliceapp/AddNewGroup.java
@@ -1,12 +1,21 @@
 package it.polito.mad17.viral.sliceapp;
 
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Build;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.Manifest;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,7 +28,8 @@ import java.util.Map;
 
 public class AddNewGroup extends AppCompatActivity {
 
-    private static final int REQUEST_CODE_PICK_CONTACTS = 1;
+
+    private static final int PERMISSION_REQUEST_CONTACT = 1;
     private static final String TAG = AddNewGroup.class.getSimpleName();
  //   private ArrayList<Persona> contacts = new ArrayList<Persona>();
     private Map<String,Persona> contactsMap = new HashMap<String,Persona>();
@@ -28,74 +38,144 @@ public class AddNewGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_group);
-
+askForContactPermission();
        // Log.d(TAG, "Response: " + data.toString());
 
-       Button b = (Button) findViewById(R.id.phoneBook);
 
-       b.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-
-               Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-               while(phones.moveToNext()) {
-                   String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                   String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-/*I made this control to manage the situation in which there is a number with +39, 39 and withou 39
-Example: +393466950530, 3466950530, 393466950530. In this case these numbers are the same, but the map
-does not understand this. That's the reason why I made this control*/
-                 //  Log.d("TruePhoneNumber",phoneNumber);
-                   if(phoneNumber.contains("+")){
-                       phoneNumber.replace("+"," ");
-                       Log.d("PIU",phoneNumber);
-                   }else{
-                       StringBuilder s = new StringBuilder();
-                       s.append("39");
-                       s.append(phoneNumber);
-                       phoneNumber = s.toString();
-                       Log.d("SENZA",phoneNumber);
-                   }
-                   phoneNumber = phoneNumber.replaceAll("[^0-9]", "");
-
-                   Persona p = new Persona(null,null,null,null,Long.parseLong(phoneNumber));
-                   contactsMap.put(phoneNumber,p);
-                   Log.d("Phone", phoneNumber);
-                 //  Log.d("Name", name);
-               }
-               phones.close();
-
-               //Firebase connection:
-               database = FirebaseDatabase.getInstance("https://sliceapp-a55d6.firebaseio.com/");
-               DatabaseReference users = database.getReference().child("otherusers");
-
-
-               users.addListenerForSingleValueEvent(new ValueEventListener() {
-                   @Override
-                   public void onDataChange(DataSnapshot dataSnapshot) {
-                       for(Persona p: contactsMap.values()){
-                           String telephone = String.valueOf(p.getTelephone());
-                         //  Log.d("TelefonoLista",telephone);
-                           Object userDB = dataSnapshot.child(telephone).getValue();
-                        //   Log.d("TelephoneDB",telephoneDB.getKey());
-                           if(userDB==null){
-                               //User is not into the DB
-                         //       Log.d("Falso","FALSE "+telephone);
-                                p.setisInDB(0);
-                           }else{
-                         //      Log.d("Vero","VERO "+ telephone);
-                               p.setisInDB(1); //User is into the DB.
-                           }
-                       }
-                   }
-
-                   @Override
-                   public void onCancelled(DatabaseError databaseError) {
-
-                   }
-               });
-           }
-       });
-        Log.d("Fine","SIAMO FUORI");
     }
 
+    public void askForContactPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Contacts access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("please confirm Contacts access");//TODO put real question
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.READ_CONTACTS}, PERMISSION_REQUEST_CONTACT);
+                        }
+                    });
+                    builder.show();
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            PERMISSION_REQUEST_CONTACT);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }else{
+                getContact();
+            }
+        }
+        else{
+            getContact();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CONTACT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContact();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Toast.makeText(this, "No permission for contacts", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+        public void getContact(){
+        Button b = (Button) findViewById(R.id.phoneBook);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+                while(phones.moveToNext()) {
+                    String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    if(phoneNumber.contains("+")){
+                        phoneNumber.replace("+"," ");
+                        Log.d("PIU",phoneNumber);
+                    }else{
+                        StringBuilder s = new StringBuilder();
+                        s.append("39");
+                        s.append(phoneNumber);
+                        phoneNumber = s.toString();
+                        Log.d("SENZA",phoneNumber);
+                    }
+                    phoneNumber = phoneNumber.replaceAll("[^0-9]", "");
+
+                    Persona p = new Persona(null,null,null,null,Long.parseLong(phoneNumber));
+                    contactsMap.put(phoneNumber,p);
+                    Log.d("Phone", phoneNumber);
+                    //  Log.d("Name", name);
+                }
+                phones.close();
+
+                //Firebase connection:
+                database = FirebaseDatabase.getInstance("https://sliceapp-a55d6.firebaseio.com/");
+                DatabaseReference users = database.getReference().child("otherusers");
+
+
+                users.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(Persona p: contactsMap.values()){
+                            String telephone = String.valueOf(p.getTelephone());
+                            //  Log.d("TelefonoLista",telephone);
+                            Object userDB = dataSnapshot.child(telephone).getValue();
+                            //   Log.d("TelephoneDB",telephoneDB.getKey());
+                            if(userDB==null){
+                                //User is not into the DB
+                                //       Log.d("Falso","FALSE "+telephone);
+                                p.setisInDB(0);
+                            }else{
+                                //      Log.d("Vero","VERO "+ telephone);
+                                p.setisInDB(1); //User is into the DB.
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+        Log.d("Fine","SIAMO FUORI");
+    }
 }
